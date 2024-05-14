de95f36c163a3d55690976556bf6dea3e05da7d1
Frank <f.r.harraway@student.tudelft.nl>
2024-05-14 09:08:50 +0200

    got gradients for algorithm up and running

--- inpaint.py
+++ inpaint.py
@@ -1,9 +1,22 @@
-import diplib as dp
+import numpy as np
+from  scipy.misc import derivative
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2
+def in_paint_alg(img, function,target_region,source_region,patch_size):
+    im_x=img.shape[0]
+    im_y=img.shape[1]
+    C=np.zeros((im_x,im_y))
+    
 
-print("Hello, World!")
 
-im1=dp.ImageRead("pout (1).tif")
-im1.Show()
 
-input("Press any key to exit...")
+
+# # Define your continuous function
+# def f(x):
+#     return x
+
+# # Define the derivative function
+# def df(x):
+#     return derivative(f, x, dx=1e-8)
 
